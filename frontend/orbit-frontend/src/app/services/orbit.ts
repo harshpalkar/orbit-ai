305f7057d2c1efab1bdf63185e17b7606a0de804
Harsh Palkar <explorewitharsh@outlook.com>
2025-11-10 16:46:38 +0530

    api url changed to render url:

--- frontend/orbit-frontend/src/app/services/orbit.ts
+++ frontend/orbit-frontend/src/app/services/orbit.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class OrbitService {
-  private apiUrl = 'http://127.0.0.1:8000/chat'; // FastAPI backend
+  private apiUrl = 'https://orbit-ai-ob63.onrender.com'; // FastAPI backend
 
   constructor(private http: HttpClient) {}
 
